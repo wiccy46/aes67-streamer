1576cbb2f9b2c690387f173ef1920f2045ffa6d5
Jiajun <eric1986_chn@yahoo.com>
2025-11-27 13:03:45 +0900

    SAP announcer

--- src/network/src/lib.rs
+++ src/network/src/lib.rs
@@ -1,7 +1,9 @@
 pub mod rtp;
 pub mod socket;
+pub mod sap;
 
 pub use rtp::{RtpPacket, RtpHeader, RtpPacketizer};
 pub use socket::{MulticastSocket, MulticastConfig, SocketStats, resolve_interface_ip};
+pub use sap::SapAnnouncer;
 
 pub type Result<T> = anyhow::Result<T>;
